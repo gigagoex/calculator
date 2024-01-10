3e987596fc93d441ce39c41fd58ee4735c702b2b
Maximilian Göckeler <maximilian.goeckeler@t-online.de>
2024-01-10 14:59:20 +0100

    double inputs are now calculated correctly

---
+++ src/test/java/CalculatorTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+
+    @Test
+    public void solveTerm() {
+    }
+
+    @Test
+    public void solveSplitTerm() {
+    }
+
+    @Test
+    public void calculateFromLeftToRight() {
+    }
+
+    @Test
+    public void divide() {
+    }
+
+    @Test
+    public void deleteUsedTerm() {
+    }
+
+    @Test
+    public void printResult() {
+    }
+}
