67e47aae0831682d5fecb6ac71460db5785cf915
Maximilian Göckeler <maximilian.goeckeler@t-online.de>
2024-01-31 13:08:46 +0100

    added description

--- src/main/java/com/github/gigagoex/exercises/exceptions/DividedByZeroException.java
+++ src/main/java/com/github/gigagoex/exercises/exceptions/DividedByZeroException.java
@@ -1,5 +1,8 @@
 package com.github.gigagoex.exercises.exceptions;
 
+/**
+ * Exception for division by 0
+ */
 public class DividedByZeroException extends Exception{
     public DividedByZeroException(){
         super("Divided by zero!");
--- src/main/java/com/github/gigagoex/exercises/exceptions/InvalidInputFormatException.java
+++ src/main/java/com/github/gigagoex/exercises/exceptions/InvalidInputFormatException.java
@@ -1,5 +1,8 @@
 package com.github.gigagoex.exercises.exceptions;
 
+/**
+ * Exception for Inputs which deviate from the required input format
+ */
 public class InvalidInputFormatException extends Exception{
     public InvalidInputFormatException(String str){
         super("Invalid Input: " + str);
